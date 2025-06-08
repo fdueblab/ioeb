87efeee736363ca17301259f10c07b5d2ed11aa7
PolarSnowLeopard <1365240381@qq.com>
2025-06-08 20:42:31 +0800

    docs: 基础路径配置

--- docs/.vitepress/config.mjs
+++ docs/.vitepress/config.mjs
@@ -2,6 +2,8 @@ import { defineConfig } from 'vitepress'
 
 // https://vitepress.dev/reference/site-config
 export default defineConfig({
+  base: '/docs/',
+  
   title: "模型智能体平台",
   description: "复旦大学计算与智能创新学院电子商务实验室",
   themeConfig: {
