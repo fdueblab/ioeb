1d61307d76c06f778cd3574bf0e8c185949c6d97
PolarSnowLeopard <1365240381@qq.com>
2025-06-16 18:10:17 +0800

    docs：添加MCP技术文档

--- docs/.vitepress/config.mjs
+++ docs/.vitepress/config.mjs
@@ -143,6 +143,12 @@ export default defineConfig({
             // { text: '代码规范', link: '/dev/code-standards' }
           ]
         },
+        {
+          text: '技术文档',
+          items:[
+            { text: '模型上下文协议', link: '/dev/mcp-intro' },
+          ]
+        }
         // {
         //   text: '架构与设计',
         //   items: [
